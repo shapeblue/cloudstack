a3760910a81c87fe3557999114e793b78947520b
Pierre-Luc Dion <pdion891@apache.org>
2017-07-31 10:27:20 -0400

    Updating pom.xml version numbers for release 4.10.0.219
    Signed-off-by: Pierre-Luc Dion <pdion891@apache.org>

--- tools/marvin/setup.py
+++ tools/marvin/setup.py
@@ -27,7 +27,7 @@ except ImportError:
         raise RuntimeError("python setuptools is required to build Marvin")
 
 
-VERSION = "4.10.0.218"
+VERSION = "4.10.0.219"
 
 setup(name="Marvin",
       version=VERSION,
