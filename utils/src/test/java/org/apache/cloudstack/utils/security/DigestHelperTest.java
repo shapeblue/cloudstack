370b03f7fea2b209ecc7f730c3875f8867e424ab
Daan Hoogland <daan.hoogland@shapeblue.com>
2017-08-09 14:51:47 +0200

    add \n to tests to match file based tool

--- utils/src/test/java/org/apache/cloudstack/utils/security/DigestHelperTest.java
+++ utils/src/test/java/org/apache/cloudstack/utils/security/DigestHelperTest.java
@@ -1,34 +1,34 @@
 package org.apache.cloudstack.utils.security;
 
+import java.io.InputStream;
+
 import com.amazonaws.util.StringInputStream;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.InputStream;
-
 public class DigestHelperTest {
 
-    private final String s = "01234567890123456789012345678901234567890123456789012345678901234567890123456789";
+    private final String s = "01234567890123456789012345678901234567890123456789012345678901234567890123456789\n";
 
     @Test
     public void testDigestSHA256() throws Exception {
         InputStream is = new StringInputStream(s);
         String result = DigestHelper.digest("SHA-256", is);
-        Assert.assertEquals("{SHA-256}af1909413b96cbb29927b3a67f3a8879c801a37be383e5f9b31df5fa8d10fa2b", result);
+        Assert.assertEquals("{SHA-256}c6ab15af7842d23d3c06c138b53a7d09c5e351a79c4eb3c8ca8d65e5ce8900ab", result);
     }
 
     @Test
     public void testDigestSHA1() throws Exception {
         InputStream is = new StringInputStream(s);
         String result = DigestHelper.digest("SHA-1", is);
-        Assert.assertEquals("{SHA-1}db9d100073836c9651690af5a74192fe6af1a2b6", result);
+        Assert.assertEquals("{SHA-1}49e4b2f4292b63e88597c127d11bc2cc0f2ca0ff", result);
     }
 
     @Test
     public void testDigestMD5() throws Exception {
         InputStream is = new StringInputStream(s);
         String result = DigestHelper.digest("MD5", is);
-        Assert.assertEquals("{MD5}faef1f4cb01d560d59016a2d5e91da6", result);
+        Assert.assertEquals("{MD5}d141a8eeaf6bba779d1d1dc5102a81c5", result);
     }
 }
 
