500bbf8fcb81748c5d0c3a133724c8673ccdf110
Syed <syed1.mushtaq@gmail.com>
2017-09-13 17:01:36 -0400

    Updating pom.xml version numbers for release 4.10.0.224
    Signed-off-by: Syed <syed1.mushtaq@gmail.com>

--- tools/marvin/setup.py
+++ tools/marvin/setup.py
@@ -27,7 +27,7 @@ except ImportError:
         raise RuntimeError("python setuptools is required to build Marvin")
 
 
-VERSION = "4.10.0.223"
+VERSION = "4.10.0.224"
 
 setup(name="Marvin",
       version=VERSION,
