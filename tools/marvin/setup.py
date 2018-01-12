49033063013639b0841ea5956263565c5d714259
Khosrow Moossavi <kmoossavi@cloudops.com>
2018-01-12 14:03:17 -0500

    Updating pom.xml version numbers for release 4.10.0.227
    Signed-off-by: Khosrow Moossavi <kmoossavi@cloudops.com>

--- tools/marvin/setup.py
+++ tools/marvin/setup.py
@@ -27,7 +27,7 @@ except ImportError:
         raise RuntimeError("python setuptools is required to build Marvin")
 
 
-VERSION = "4.10.0.226"
+VERSION = "4.10.0.227"
 
 setup(name="Marvin",
       version=VERSION,
