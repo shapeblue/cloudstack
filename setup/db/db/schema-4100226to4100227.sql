157f9da44cebda4a5ba56333445310a6bb2f5d63
Khosrow Moossavi <kmoossavi@cloudops.com>
2018-04-20 13:59:55 -0400

    Add schema upgrade for VDI-per-LUN (4.10.0.227)

--- setup/db/db/schema-4100226to4100227.sql
+++ setup/db/db/schema-4100226to4100227.sql
@@ -18,3 +18,50 @@
 --;
 -- Schema upgrade from 4.10.0.226 to 4.10.0.227;
 --;
+
+-- VDI-per-LUN
+ALTER TABLE `cloud`.`disk_offering` ADD COLUMN `min_iops_per_gb`  int unsigned DEFAULT NULL COMMENT 'Min IOPS per GB';
+ALTER TABLE `cloud`.`disk_offering` ADD COLUMN `max_iops_per_gb`  int unsigned DEFAULT NULL COMMENT 'Max IOPS per GB';
+ALTER TABLE `cloud`.`disk_offering` ADD COLUMN `highest_min_iops` int unsigned DEFAULT NULL COMMENT 'Highest Min IOPS for the offering';
+ALTER TABLE `cloud`.`disk_offering` ADD COLUMN `highest_max_iops` int unsigned DEFAULT NULL COMMENT 'Highest Max IOPS for the offering';
+DROP VIEW IF EXISTS `cloud`.`disk_offering_view`;
+CREATE VIEW `cloud`.`disk_offering_view` AS
+    select
+        disk_offering.id,
+        disk_offering.uuid,
+        disk_offering.name,
+        disk_offering.display_text,
+        disk_offering.provisioning_type,
+        disk_offering.disk_size,
+        disk_offering.min_iops,
+        disk_offering.max_iops,
+        disk_offering.created,
+        disk_offering.tags,
+        disk_offering.customized,
+        disk_offering.customized_iops,
+        disk_offering.removed,
+        disk_offering.use_local_storage,
+        disk_offering.system_use,
+        disk_offering.hv_ss_reserve,
+        disk_offering.bytes_read_rate,
+        disk_offering.bytes_write_rate,
+        disk_offering.iops_read_rate,
+        disk_offering.iops_write_rate,
+        disk_offering.min_iops_per_gb,
+        disk_offering.max_iops_per_gb,
+        disk_offering.highest_min_iops,
+        disk_offering.highest_max_iops,
+        disk_offering.cache_mode,
+        disk_offering.sort_key,
+        disk_offering.type,
+        disk_offering.display_offering,
+        domain.id domain_id,
+        domain.uuid domain_uuid,
+        domain.name domain_name,
+        domain.path domain_path
+    from
+        `cloud`.`disk_offering`
+            left join
+        `cloud`.`domain` ON disk_offering.domain_id = domain.id
+    where
+        disk_offering.state='ACTIVE';
