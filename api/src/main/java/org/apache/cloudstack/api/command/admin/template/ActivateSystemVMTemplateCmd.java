68eeb0227d7fc7897e121ce46f32a04cf4b360be
ernjvr <ernjvr@gmail.com>
2018-10-11 12:23:29 +0200

    created new api skeletons and some implementation logic for it

---
+++ api/src/main/java/org/apache/cloudstack/api/command/admin/template/ActivateSystemVMTemplateCmd.java
@@ -0,0 +1,99 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+package org.apache.cloudstack.api.command.admin.template;
+
+import com.cloud.exception.ConcurrentOperationException;
+import com.cloud.exception.InsufficientCapacityException;
+import com.cloud.exception.NetworkRuleConflictException;
+import com.cloud.exception.ResourceAllocationException;
+import com.cloud.exception.ResourceUnavailableException;
+import com.cloud.template.VirtualMachineTemplate;
+import com.cloud.user.Account;
+import org.apache.cloudstack.acl.RoleType;
+import org.apache.cloudstack.api.APICommand;
+import org.apache.cloudstack.api.ApiCommandJobType;
+import org.apache.cloudstack.api.ApiConstants;
+import org.apache.cloudstack.api.ApiErrorCode;
+import org.apache.cloudstack.api.BaseCmd;
+import org.apache.cloudstack.api.Parameter;
+import org.apache.cloudstack.api.ResponseObject;
+import org.apache.cloudstack.api.ServerApiException;
+import org.apache.cloudstack.api.response.ListResponse;
+import org.apache.cloudstack.api.response.TemplateResponse;
+import org.apache.log4j.Logger;
+
+import java.util.List;
+
+@APICommand(name = "activateSystemVMTemplate", description = "Activates an existing system virtual machine template to be used by CloudStack to create system virtual machines.",
+        responseObject = TemplateResponse.class, authorized = {RoleType.Admin})
+public class ActivateSystemVMTemplateCmd extends BaseCmd {
+
+    public static final Logger LOGGER = Logger.getLogger(ActivateSystemVMTemplateCmd.class.getName());
+    private static final String NAME = "activatesystemvmtemplateresponse";
+
+    /////////////////////////////////////////////////////
+    //////////////// API parameters /////////////////////
+    /////////////////////////////////////////////////////
+
+    @Parameter(name = ApiConstants.ID, type = CommandType.UUID,
+            entityType = TemplateResponse.class, required = true, description = "The template ID of the System VM Template to activate.")
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void execute() throws ResourceUnavailableException, InsufficientCapacityException, ServerApiException, ConcurrentOperationException, ResourceAllocationException, NetworkRuleConflictException {
+//        try {
+            VirtualMachineTemplate template = _templateService.activateSystemVMTemplate(this);
+            if (template != null) {
+                ListResponse<TemplateResponse> response = new ListResponse<TemplateResponse>();
+                List<TemplateResponse> templateResponses = _responseGenerator.createTemplateResponses(ResponseObject.ResponseView.Restricted,
+                        template, 1L , false);
+                response.setResponses(templateResponses);
+                response.setResponseName(getCommandName());
+                setResponseObject(response);
+            } else {
+                throw new ServerApiException(ApiErrorCode.INTERNAL_ERROR, "Failed to activate template.");
+            }
+//        } catch ( ex) {
+//            LOGGER.warn("Exception: ", ex);
+//            throw new ServerApiException(ApiErrorCode.INTERNAL_ERROR, ex.getMessage());
+//        }
+    }
+
+    @Override
+    public String getCommandName() {
+        return NAME;
+    }
+
+    public ApiCommandJobType getInstanceType() {
+        return ApiCommandJobType.Template;
+    }
+
+    @Override
+    public long getEntityOwnerId() {
+        final VirtualMachineTemplate template = _entityMgr.findById(VirtualMachineTemplate.class, id);
+        if (template != null) {
+            return template.getAccountId();
+        }
+
+        // bad id given, parent this command to SYSTEM so ERROR events are tracked
+        return Account.ACCOUNT_ID_SYSTEM;
+    }
+}
