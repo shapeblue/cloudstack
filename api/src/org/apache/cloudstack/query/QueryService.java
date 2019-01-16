85ce902f9ed81577e798af5836d7c22fa7faeff1
Rohit Yadav <rohit.yadav@shapeblue.com>
2019-01-16 16:00:59 +0530

    Update QueryService.java

--- api/src/org/apache/cloudstack/query/QueryService.java
+++ api/src/org/apache/cloudstack/query/QueryService.java
@@ -85,9 +85,9 @@ public interface QueryService {
     static final ConfigKey<Boolean> AllowUserViewDestroyedVM = new ConfigKey<Boolean>("Advanced", Boolean.class, "allow.user.view.destroyed.vm", "false",
             "Determines whether users can view their destroyed or expunging vm ", true, ConfigKey.Scope.Account);
 
-    static final ConfigKey<String> HideUserViewVMSettings = new ConfigKey<String>("Advanced", String.class,
-            "vm.details.user.blacklist.keys", "rootdisksize, cpuOvercommitRatio, memoryOvercommitRatio, Message.ReservedCapacityFreed.flag",
-            "Determines whether users can view certain vm settings", true);
+    static final ConfigKey<String> UserVMBlacklistedDetails = new ConfigKey<String>("Advanced", String.class,
+            "user.vm.blacklisted.details", "rootdisksize, cpuOvercommitRatio, memoryOvercommitRatio, Message.ReservedCapacityFreed.flag",
+            "Determines whether users can view certain VM settings", true);
 
 
     ListResponse<UserResponse> searchForUsers(ListUsersCmd cmd) throws PermissionDeniedException;
