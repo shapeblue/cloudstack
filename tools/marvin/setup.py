8a4c6f199d5b0102e988518bec322c6f5d2e0d5f
Khosrow Moossavi <kmoossavi@cloudops.com>
2019-10-04 13:14:21 -0400

    Updating pom.xml version numbers for release 4.12.0.2-cca-SNAPSHOT
    Signed-off-by: Khosrow Moossavi <kmoossavi@cloudops.com>

--- tools/marvin/setup.py
+++ tools/marvin/setup.py
@@ -27,7 +27,7 @@ except ImportError:
         raise RuntimeError("python setuptools is required to build Marvin")
 
 
-VERSION = "4.12.0.1-cca"
+VERSION = "4.12.0.2-cca-SNAPSHOT"
 
 setup(name="Marvin",
       version=VERSION,
