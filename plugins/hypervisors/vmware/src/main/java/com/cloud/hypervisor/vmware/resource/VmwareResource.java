9a4ecb3c8992307acc47fa0f8b08298fb0ae1fd2
Rohit Yadav <rohit.yadav@shapeblue.com>
2020-04-02 14:27:07 +0530

    poc: for vapp enabled VMs add VMware equivalent of config drive
    Signed-off-by: Rohit Yadav <rohit.yadav@shapeblue.com>

--- plugins/hypervisors/vmware/src/main/java/com/cloud/hypervisor/vmware/resource/VmwareResource.java
+++ plugins/hypervisors/vmware/src/main/java/com/cloud/hypervisor/vmware/resource/VmwareResource.java
@@ -1949,6 +1949,25 @@ public class VmwareResource implements StoragePoolResource, ServerResource, Vmwa
             // Setup ISO device
             //
 
+            // vAPP ISO
+            if (vmSpec.getOvfProperties() != null) {
+                deviceConfigSpecArray[i] = new VirtualDeviceConfigSpec();
+                Pair<VirtualDevice, Boolean> isoInfo = VmwareHelper.prepareIsoDevice(vmMo, null, null, true, true, ideUnitNumber++, i + 1);
+                deviceConfigSpecArray[i].setDevice(isoInfo.first());
+                if (isoInfo.second()) {
+                    if (s_logger.isDebugEnabled())
+                        s_logger.debug("Prepare vApp ISO volume at existing device " + _gson.toJson(isoInfo.first()));
+
+                    deviceConfigSpecArray[i].setOperation(VirtualDeviceConfigSpecOperation.ADD);
+                } else {
+                    if (s_logger.isDebugEnabled())
+                        s_logger.debug("Prepare vApp ISO volume at existing device " + _gson.toJson(isoInfo.first()));
+
+                    deviceConfigSpecArray[i].setOperation(VirtualDeviceConfigSpecOperation.EDIT);
+                }
+                i++;
+            }
+
             // prepare systemvm patch ISO
             if (vmSpec.getType() != VirtualMachine.Type.User) {
                 // attach ISO (for patching of system VM)
