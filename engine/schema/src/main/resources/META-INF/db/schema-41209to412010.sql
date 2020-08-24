46b992c405dbf9a6a08aa9ff8763b6279e834479
Serge Nassar <snassar@cloud.ca>
2020-08-24 12:30:50 -0400

    Added xenserver 8.2 inserts

--- engine/schema/src/main/resources/META-INF/db/schema-41209to412010.sql
+++ engine/schema/src/main/resources/META-INF/db/schema-41209to412010.sql
@@ -22,3 +22,11 @@
 -- Adding Pxe Boot variables
 ALTER TABLE `cloud`.`vm_template` ADD `boot_filename` varchar(255) NULL default NULL COMMENT 'the url where the template exists externally';
 ALTER TABLE `cloud`.`vpc` ADD `network_boot_ip` char(40) NULL default NULL COMMENT 'Network Boot Ip';
+
+-- Add XenServer 8.2 hypervisor capabilities
+INSERT IGNORE INTO `cloud`.`hypervisor_capabilities`(uuid, hypervisor_type, hypervisor_version, max_guests_limit, max_data_volumes_limit, storage_motion_supported)
+values (UUID(), 'Citrix Hypervisor', '8.2.0', 500, 13, 1);
+
+-- Copy XenServer 8.1 hypervisor guest OS mappings to XenServer 8.2
+INSERT IGNORE INTO `cloud`.`guest_os_hypervisor` (uuid,hypervisor_type, hypervisor_version, guest_os_name, guest_os_id, created, is_user_defined)
+SELECT UUID(),'Citrix Hypervisor', '8.2.0', guest_os_name, guest_os_id, utc_timestamp(), 0  FROM `cloud`.`guest_os_hypervisor` WHERE hypervisor_type='Citrix Hypervisor' AND hypervisor_version='8.1.0';
