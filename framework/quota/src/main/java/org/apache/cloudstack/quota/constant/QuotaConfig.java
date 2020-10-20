29e89761f363981dd28ebfdfad459088ca1c6346
Pearl Dsilva <pearl.dsilva@shapeblue.com>
2020-10-20 22:46:16 +0530

    Make the quota enable service global setting non dynamic

--- framework/quota/src/main/java/org/apache/cloudstack/quota/constant/QuotaConfig.java
+++ framework/quota/src/main/java/org/apache/cloudstack/quota/constant/QuotaConfig.java
@@ -22,7 +22,7 @@ import org.apache.cloudstack.framework.config.ConfigKey;
 public interface QuotaConfig {
 
     public static final ConfigKey<Boolean> QuotaPluginEnabled = new ConfigKey<Boolean>("Advanced", Boolean.class, "quota.enable.service", "false",
-            "Indicates whether Quota plugin is enabled or not", true);
+            "Indicates whether Quota plugin is enabled or not", false);
 
     public static final ConfigKey<String> QuotaEnableEnforcement = new ConfigKey<String>("Advanced", String.class, "quota.enable.enforcement", "false",
             "Enable the usage quota enforcement, i.e. on true when exceeding quota the respective account will be locked.", true);
