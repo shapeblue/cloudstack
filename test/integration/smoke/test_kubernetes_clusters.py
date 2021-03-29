5c6f59a6f85ced07f80b0402f3d336addb2b0408
Pearl Dsilva <pearl.dsilva@shapeblue.com>
2021-03-29 17:56:19 +0530

    Fix k8s tests - enable full clone in case of resize of volumes

--- test/integration/smoke/test_kubernetes_clusters.py
+++ test/integration/smoke/test_kubernetes_clusters.py
@@ -37,6 +37,7 @@ from marvin.codes import PASS, FAILED
 from marvin.lib.base import (Template,
                              ServiceOffering,
                              Account,
+                             StoragePool,
                              Configurations)
 from marvin.lib.utils import (cleanup_resources,
                               validateList,
@@ -81,6 +82,20 @@ class TestKubernetesCluster(cloudstackTestCase):
                                       "cloud.kubernetes.service.enabled",
                                       "true")
                 cls.restartServer()
+            if cls.hypervisor.lower() == 'vmware':
+                cls.create_full_clone = Configurations.list(cls.apiclient, name="vmware.create.full.clone")[0].value
+                if cls.create_full_clone not in ["true", True]:
+                    Configurations.update(cls.apiclient,
+                                          "vmware.create.full.clone",
+                                          "true")
+                allStoragePools = StoragePool.list(
+                    cls.apiclient
+                )
+                for pool in allStoragePools:
+                    Configurations.update(cls.apiclient,
+                                          storageid=pool.id,
+                                          name="vmware.create.full.clone",
+                                          value="true")
 
             cls.cks_template = None
             cls.initial_configuration_cks_template_name = None
@@ -175,6 +190,21 @@ class TestKubernetesCluster(cloudstackTestCase):
                                       "cloud.kubernetes.service.enabled",
                                       "false")
                 cls.restartServer()
+
+            if cls.hypervisor.lower() == 'vmware':
+                cls.create_full_clone = Configurations.list(cls.apiclient, name="vmware.create.full.clone")[0].value
+                if cls.create_full_clone in ["true", True]:
+                    Configurations.update(cls.apiclient,
+                                          "vmware.create.full.clone",
+                                          "false")
+                allStoragePools = StoragePool.list(
+                    cls.apiclient
+                )
+                for pool in allStoragePools:
+                    Configurations.update(cls.apiclient,
+                                          storageid=pool.id,
+                                          name="vmware.create.full.clone",
+                                          value="false")
 
             cleanup_resources(cls.apiclient, cls._cleanup)
         except Exception as e:
