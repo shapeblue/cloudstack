47685ef9ee200e8603f6ba52fee88e9ffa4b0d3f
Harikrishna Patnala <harikrishna.patnala@gmail.com>
2021-08-05 13:29:10 +0530

    Fix UI conflicts

--- ui/src/views/offering/AddDiskOffering.vue
+++ ui/src/views/offering/AddDiskOffering.vue
@@ -75,6 +75,15 @@
           </a-radio-group>
         </a-form-item>
         <a-form-item>
+          <span slot="label">
+            {{ $t('label.disksizestrictness') }}
+            <a-tooltip :title="apiParams.disksizestrictness.description">
+              <a-icon type="info-circle" style="color: rgba(0,0,0,.45)" />
+            </a-tooltip>
+          </span>
+          <a-switch v-decorator="['disksizestrictness', { initialValue: this.disksizestrictness }]" :checked="this.disksizestrictness" @change="val => { this.disksizestrictness = val }" />
+        </a-form-item>
+        <a-form-item>
           <tooltip-label slot="label" :title="$t('label.customdisksize')" :tooltip="apiParams.customized.description"/>
           <a-switch v-decorator="['customdisksize', { initialValue: isCustomDiskSize }]" :checked="isCustomDiskSize" @change="val => { isCustomDiskSize = val }" />
         </a-form-item>
@@ -378,7 +387,8 @@ export default {
       domainLoading: false,
       zones: [],
       zoneLoading: false,
-      loading: false
+      loading: false,
+      disksizestrictness: false
     }
   },
   beforeCreate () {
@@ -500,7 +510,8 @@ export default {
           storageType: values.storagetype,
           cacheMode: values.writecachetype,
           provisioningType: values.provisioningtype,
-          customized: values.customdisksize
+          customized: values.customdisksize,
+          disksizestrictness: values.disksizestrictness
         }
         if (values.customdisksize !== true) {
           params.disksize = values.disksize
