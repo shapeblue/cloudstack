1c1c14ad2a8fb24963d32832a27f15162af9a078
Abhishek Kumar <abhishek.mrt22@gmail.com>
2021-08-26 16:01:35 +0530

    add logs
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- plugins/hypervisors/vmware/src/main/java/com/cloud/storage/resource/VmwareStorageProcessor.java
+++ plugins/hypervisors/vmware/src/main/java/com/cloud/storage/resource/VmwareStorageProcessor.java
@@ -1848,6 +1848,7 @@ public class VmwareStorageProcessor implements StorageProcessor {
                     s_logger.error(msg);
                     throw new Exception(msg);
                 }
+                s_logger.debug(String.format("-----------------------------------Cloned VM: %s as %s", vmMo.getName(), clonedVm.getName()));
                 vmMo = clonedVm;
             }
             vmMo.exportVm(exportPath, exportName, false, false);
@@ -1855,6 +1856,7 @@ public class VmwareStorageProcessor implements StorageProcessor {
             return new Pair<>(diskDevice, disks);
         } finally {
             if (clonedVm != null) {
+                s_logger.debug(String.format("-----------------------------------Destroying cloned VM: %s with its disks", clonedVm.getName()));
                 // clonedVm.detachAllDisks();
                 clonedVm.destroy();
             }
