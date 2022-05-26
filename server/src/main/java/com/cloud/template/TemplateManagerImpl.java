68f083d11e123b6ca3a8fa4aefd0eddf25957949
Harikrishna Patnala <harikrishna.patnala@gmail.com>
2022-05-26 16:38:21 +0530

    Added blank values for userdata in edit template form

--- server/src/main/java/com/cloud/template/TemplateManagerImpl.java
+++ server/src/main/java/com/cloud/template/TemplateManagerImpl.java
@@ -2295,7 +2295,11 @@ public class TemplateManagerImpl extends ManagerBase implements TemplateManager,
         _accountMgr.checkAccess(caller, AccessType.OperateEntry, true, template);
 
         template.setUserDataId(userDataId);
-        template.setUserDataLinkPolicy(overridePolicy);
+        if (userDataId != null) {
+            template.setUserDataLinkPolicy(overridePolicy);
+        } else {
+            template.setUserDataLinkPolicy(null);
+        }
         _tmpltDao.update(templateId, template);
 
         return _tmpltDao.findById(templateId);
