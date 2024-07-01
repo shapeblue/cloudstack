c3007936385b0eae342e085a58ad6fa0683aab56
dahn <daan.hoogland@gmail.com>
2024-07-01 16:58:17 +0200

    Update client/src/main/java/org/apache/cloudstack/ServerDaemon.java

--- client/src/main/java/org/apache/cloudstack/ServerDaemon.java
+++ client/src/main/java/org/apache/cloudstack/ServerDaemon.java
@@ -308,7 +308,7 @@ public class ServerDaemon implements Daemon {
     }
 
     private void createRotateFile(File logPath) {
-        String rotatefile = "/etc/logrotate/access";
+        String rotatefile = "/etc/logrotate.d/access";
         String fileContents = logPath.getAbsolutePath() + " {\n"
                 + "  copytruncate"
                 + "  daily"
