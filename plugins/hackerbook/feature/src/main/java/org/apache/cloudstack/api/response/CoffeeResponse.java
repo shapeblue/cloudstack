f5ff07450547b1421f740440b7f7d688b6a63fd9
Daman Arora <daman.arora@shapeblue.com>
2025-11-26 17:40:05 -0500

    code cleanup
    Signed-off-by: Daman Arora <daman.arora@shapeblue.com>

--- plugins/hackerbook/feature/src/main/java/org/apache/cloudstack/api/response/CoffeeResponse.java
+++ plugins/hackerbook/feature/src/main/java/org/apache/cloudstack/api/response/CoffeeResponse.java
@@ -1,31 +1,50 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
 package org.apache.cloudstack.api.response;
 
 import com.google.gson.annotations.SerializedName;
 import org.apache.cloudstack.api.BaseResponse;
 import org.apache.cloudstack.api.EntityReference;
 import org.apache.cloudstack.api.Coffee;
+import org.apache.cloudstack.api.ApiConstants;
 import com.cloud.serializer.Param;
 
 @EntityReference(value = Coffee.class)
 public class CoffeeResponse extends BaseResponse {
+    public static final String OFFERING = "offering";
+    public static final String SIZE = "size";
 
-    @SerializedName("id")
+    @SerializedName(ApiConstants.ID)
     @Param(description = "the coffee ID")
     private String id;
 
-    @SerializedName("name")
+    @SerializedName(ApiConstants.NAME)
     @Param(description = "the coffee name")
     private String name;
 
-    @SerializedName("offering")
+    @SerializedName(OFFERING)
     @Param(description = "the type of coffee")
     private String offering;
 
-    @SerializedName("size")
+    @SerializedName(SIZE)
     @Param(description = "the size of coffee")
     private String size;
 
-    @SerializedName("state")
+    @SerializedName(ApiConstants.STATE)
     @Param(description = "current coffee state")
     private String state;
 
